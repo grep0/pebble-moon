36c6111ec532a4f6dba0f16c2942ddecd0c7a830
Maxim Razin <maxim.razin@gmail.com>
2014-03-09 21:33:20 -0700

    implement hemisphere for correct moon phase pictures

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,14 +1,27 @@
-var send_tz = function() {
-  var d = new Date();
-  var tz_sec = -60 * d.getTimezoneOffset();
-  var hemisphere = 1;
-  console.log("tz_sec=" + tz_sec + " hemisphere=" + hemisphere);
-  Pebble.sendAppMessage({"tz_offset": tz_sec, "hemisphere": hemisphere},
+var send_pebble = function(tz_sec, loc) {
+  msg = {"tz_offset": tz_sec};
+  if (loc) {
+    msg.hemisphere = (loc.coords.latitude >= 0) ? 0 : 1;
+  }
+  console.log("msg.tz_offset=" + msg.tz_offset + " hemisphere=" + msg.hemisphere);
+  Pebble.sendAppMessage(msg,
     function(e) {
       console.log("TimeZone info sent");
     },
     function(e) {
       console.log("TimeZone info send error: " + e.error.message);
+    });  
+}
+
+var send_tz = function() {
+  var d = new Date();
+  var tz_sec = -60 * d.getTimezoneOffset();
+  navigator.geolocation.getCurrentPosition(
+    function(loc) {
+      send_pebble(tz_sec, loc);
+    },
+    function(loc) {
+      send_pebble(tz_sec, null);
     });
 }
 
